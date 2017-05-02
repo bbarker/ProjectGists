709d0cd6544295f7b9069bb867d964f9aa740554
bbarker <beb82@cornell.edu>
2017-05-01 22:26:37 -0400

    adding Squants typeclass usage example

---
+++ Scala/SquantsConversions/src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+import squants.time._
+import squants.time.TimeConversions._
+
+
+class Main {
+  var conceptionTime: Time = -1 nanoseconds
+}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val thisMain = new Main
+  }
+
+}
