4b52f5c64de897531cdb7739ff72efb5c728bd16
Brandon Elam Barker <beb82@cornell.edu>
2018-03-28 20:01:27 +0000

    this also works

--- Scala/ScalaCacheConcurrentNestedCall/src/main/scala/com/foo/bar/Main.scala
+++ Scala/ScalaCacheConcurrentNestedCall/src/main/scala/com/foo/bar/Main.scala
@@ -11,16 +11,13 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    implicit val caffeineCache: Cache[String] = CaffeineCache[String]
+    implicit val caffeineCache: Cache[Future[String]] = CaffeineCache[Future[String]]
 
     val cpt = new java.util.concurrent.atomic.AtomicInteger()
     def stringF: Future[String] = Future{ cpt.incrementAndGet(); "titi"}
 
-    def outerFun: String = Await.result(stringF, Inf)
 
-
-
-    Seq.fill(1000)(0).map(_ => caching("Toto")(ttl = None)(outerFun))
+    Seq.fill(1000)(0).map(_ => caching("Toto")(ttl = None)(stringF))
 
 
     println(cpt.get) //returns 1
